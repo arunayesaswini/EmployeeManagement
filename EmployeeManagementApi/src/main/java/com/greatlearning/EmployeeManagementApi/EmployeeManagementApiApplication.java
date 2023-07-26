83c708ae17a76d690c2f93d80fdb9377bedb9d49
arunayesaswini <127669982+arunayesaswini@users.noreply.github.com>
2023-07-26 19:31:39 +0530

    Add files via upload

---
+++ EmployeeManagementApi/src/main/java/com/greatlearning/EmployeeManagementApi/EmployeeManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.greatlearning.EmployeeManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApiApplication.class, args);
+	}
+
+}
