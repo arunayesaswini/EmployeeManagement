26df8181397226e8a8917bf994bdbcf5af6b57fd
arunayesaswini <cherukupalle@gmail.com>
2023-07-28 11:33:23 +0530

    EmployeeController add /listEmployees create EmployeeService create Employee ServiceImpl

---
+++ EmployeeManagementApi/src/main/java/com/greatlearning/EmployeeManagementApi/service/EmployeeServiceImpl.java
@@ -0,0 +1,39 @@
+package com.greatlearning.EmployeeManagementApi.service;
+
+import java.util.List;
+
+import com.greatlearning.EmployeeManagementApi.entity.Employee;
+
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Override
+	public Employee addEmployee(Employee emp) {
+	
+		return null;
+	}
+
+	@Override
+	public Employee updateEmployee(long eid, Employee emp) {
+		
+		return null;
+	}
+
+	@Override
+	public Employee getEmployee(long eid) {
+		
+		return null;
+	}
+
+	@Override
+	public List<Employee> getAllEmployees() {
+		
+		return null;
+	}
+
+	@Override
+	public void deleteEmployee(int eid) {
+		
+		
+	}
+
+}
