d912238995c59cd8168f80acbe89bfc057c2f1e4
arunayesaswini <cherukupalle@gmail.com>
2023-08-06 18:23:14 +0530

    added

--- EmployeeManagementApi/src/main/java/com/greatlearning/EmployeeManagementApi/entity/User.java
+++ EmployeeManagementApi/src/main/java/com/greatlearning/EmployeeManagementApi/entity/User.java
@@ -13,10 +13,16 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
 
 	   @Id
